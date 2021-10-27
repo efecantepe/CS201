2678dc4583b7cf422388625186ea2dc44619c2b0
Efe Can Tepe <can.tepe@ug.bilkent.edu.tr>
2021-10-27 23:36:39 +0300

    Variable commit

---
+++ 1_Introduction/SampleProgram.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class GradeBook {
+
+public:
+
+    void displayMessage( int courseNo){
+        cout << "Welcome to " << courseNo << endl;
+    }
+
+};
+
+int main(){
+    int cno;
+    GradeBook g1;
+
+    cout << "Enter course no: " ;
+    cin >> cno;
+    g1.displayMessage(cno);
+    return 0;
+}
